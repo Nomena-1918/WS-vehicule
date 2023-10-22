65998b7ca7c8627cad6ebed977be2df28b516123
nomena <vahatra.nomena@yahoo.com>
2023-10-22 11:43:07 +0300

    Etape 2 Token Fini !!!

---
+++ src/main/java/org/vehicule/wsvehicule/service/TokenResponseService.java
@@ -0,0 +1,50 @@
+package org.vehicule.wsvehicule.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.vehicule.wsvehicule.model.auth.TokenResponse;
+import org.vehicule.wsvehicule.model.auth.Utilisateur;
+import org.vehicule.wsvehicule.repository.TokenResponseRepository;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class TokenResponseService {
+    private final TokenResponseRepository tokenResponseRepository;
+
+    @Value("${custom.token.duration}")
+    private int tokenDuration;
+
+    @Autowired
+    public TokenResponseService(TokenResponseRepository tokenResponseRepository) {
+        this.tokenResponseRepository = tokenResponseRepository;
+    }
+
+    public Boolean isValidToken(String token) {
+        return tokenResponseRepository.findUtilisateurByValidToken(token).isPresent();
+    }
+
+    public Optional<TokenResponse> getValidToken(Utilisateur utilisateur) {
+        return tokenResponseRepository.findValidTokenByUtilisateur(utilisateur);
+    }
+
+    public TokenResponse insertToken(Utilisateur utilisateur, String token) {
+        TokenResponse tr = new TokenResponse(utilisateur, token);
+        Timestamp t = new Timestamp(System.currentTimeMillis()+ (long) tokenDuration *60*1000);
+        tr.setDate_expiration(t);
+
+        return tokenResponseRepository.save(tr);
+    }
+
+    public Boolean validateAuthorization(Map<String,String> headers) {
+        String token = headers.get("authorization");
+        return token != null && isValidToken(token);
+    }
+
+}
